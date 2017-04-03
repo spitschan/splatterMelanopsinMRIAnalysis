4c1a9ad55f2c28e5f0bc3f65653ca19f7347c387
Manuel Spitschan <spitschan@stanford.edu>
2017-04-03 12:05:40 +0200

    Updates
    Signed-off-by: Manuel Spitschan <spitschan@stanford.edu>

--- MaxMel_SpectraTable.m
+++ MaxMel_SpectraTable.m
@@ -1,39 +1,68 @@
 % Define the paths
 dropboxBasePath = '/Users/spitschan/Dropbox (Aguirre-Brainard Lab)';
-outDir = fullfile(pwd, 'figures');
+outDir = fullfile(pwd, 'tables');
 if ~isdir(outDir)
     mkdir(outDir);
 end
 
 theDataPaths = {'MELA_data/MelanopsinMR_fMRI/MaxLMS400pct/HERO_asb1/040716/StimulusFiles/Cache-LMSDirectedSuperMaxLMS/BoxARandomizedLongCableCStubby1_ND00/23-Mar-2016_12_31_27/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxLMS400pct/HERO_aso1/033016/StimulusFiles/Cache-LMSDirectedSuperMaxLMS/BoxARandomizedLongCableCStubby1_ND00/23-Mar-2016_12_31_27/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxLMS400pct/HERO_gka1/040116/StimulusFiles/Cache-LMSDirectedSuperMaxLMS/BoxARandomizedLongCableCStubby1_ND00/23-Mar-2016_12_31_27/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxLMS400pct/HERO_mxs1/040816/StimulusFiles/Cache-LMSDirectedSuperMaxLMS/BoxARandomizedLongCableCStubby1_ND00/23-Mar-2016_12_31_27/validation' ...
     'MELA_data/MelanopsinMR_fMRI/MaxLMSCRF/HERO_asb1/060816/StimulusFiles/Cache-LMSDirectedSuperMaxLMS/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxLMSCRF/HERO_aso1/060116/StimulusFiles/Cache-LMSDirectedSuperMaxLMS/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxLMSCRF/HERO_gka1/060616/StimulusFiles/Cache-LMSDirectedSuperMaxLMS/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxLMSCRF/HERO_mxs1/061016/StimulusFiles/Cache-LMSDirectedSuperMaxLMS/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxLMSCRF/HERO_mxs1/062816/StimulusFiles/Cache-LMSDirectedSuperMaxLMS/BoxARandomizedLongCableBStubby1_ND03CassetteB/20-Jun-2016_15_17_02/validation' ...
     'MELA_data/MelanopsinMR_fMRI/MaxMel400pct/HERO_asb1/032416/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableCStubby1_ND00/23-Mar-2016_12_31_27/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxMel400pct/HERO_aso1/032516/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableCStubby1_ND00/23-Mar-2016_12_31_27/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxMel400pct/HERO_gka1/033116/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableCStubby1_ND00/23-Mar-2016_12_31_27/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxMel400pct/HERO_mxs1/040616/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableCStubby1_ND00/23-Mar-2016_12_31_27/validation' ...
     'MELA_data/MelanopsinMR_fMRI/MaxMelCRF/HERO_asb1/060716/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
-    'MELA_data/MelanopsinMR_fMRI/SplatterControlCRF/HERO_asb1/051016/StimulusFiles/Cache-MaxMelPostreceptoralSplatterControl/BoxARandomizedLongCableCStubby1_ND00/18-Apr-2016_10_23_32/validation'};
+    'MELA_data/MelanopsinMR_fMRI/MaxMelCRF/HERO_aso1/053116/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxMelCRF/HERO_gka1/060216/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxMelCRF/HERO_mxs1/060916/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/MaxMelCRF/HERO_mxs1/061016/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/SplatterControlCRF/HERO_asb1/051016/StimulusFiles/Cache-MaxMelPostreceptoralSplatterControl/BoxARandomizedLongCableCStubby1_ND00/18-Apr-2016_10_23_32/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/SplatterControlCRF/HERO_aso1/042916/StimulusFiles/Cache-MaxMelPostreceptoralSplatterControl/BoxARandomizedLongCableCStubby1_ND00/18-Apr-2016_10_23_32/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/SplatterControlCRF/HERO_gka1/050616/StimulusFiles/Cache-MaxMelPostreceptoralSplatterControl/BoxARandomizedLongCableCStubby1_ND00/18-Apr-2016_10_23_32/validation' ...
+    'MELA_data/MelanopsinMR_fMRI/SplatterControlCRF/HERO_mxs1/050916/StimulusFiles/Cache-MaxMelPostreceptoralSplatterControl/BoxARandomizedLongCableCStubby1_ND00/18-Apr-2016_10_23_32/validation'};
 
-theStimuli = {'LMS 400%' 'LMS CRF' 'Mel 400%' 'Mel CRF' 'Splatter CRF'};
-close all;
+theStimuli = {'LMS 400%' 'LMS 400%'  'LMS 400%'  'LMS 400%'  ...
+    'LMS CRF'  'LMS CRF'  'LMS CRF'  'LMS CRF' 'LMS CRF'...
+    'Mel 400%' 'Mel 400%'  'Mel 400%'  'Mel 400%'  ...
+    'Mel CRF'  'Mel CRF'  'Mel CRF'  'Mel CRF' 'Mel CRF' ...
+    'Splatter CRF' 'Splatter CRF' 'Splatter CRF'  'Splatter CRF'};
+theObservers = {'ASB' 'ASO' 'GKA' 'MXS' ...
+    'ASB' 'ASO' 'GKA' 'MXS [1]' 'MXS [2]'...
+    'ASB' 'ASO' 'GKA' 'MXS' ...
+    'ASB' 'ASO' 'GKA' 'MXS [1]' 'MXS [2]' ...
+    'ASB' 'ASO' 'GKA' 'MXS'};
+theContrastLevels = {[400] [400] [400] [400] ...
+    [25 50 100 200 400] [25 50 100 200 400] [25 50 100 200 400] [25 50 100 200 400] [25 50 100 200 400] ...
+    [400] [400] [400] [400] ...
+    [25 50 100 200 400] [25 50 100 200 400] [25 50 100 200 400] [25 50 100 200 400] [25 50 100 200 400] ...
+    [0.25 0.5 1 2] [0.25 0.5 1 2] [0.25 0.5 1 2] [0.25 0.5 1 2]};
 
-
-theFig = figure;
+%fid = fopen(fullfile(outDir, 'TableX_Spectra.csv'), 'w');
+fprintf(fid, 'Stimulus,Observer,Nominal contrast [%s],Luminance [cd/m2],SD,Irradiance [sc td],[log10 sc td],SD,Irradiance [ph td],[log10 ph td],SD,x chromaticity,SD,y chromaticity,SD,L contrast [%s],SD,M contrast [%s],SD,S contrast [%s],SD,Melanopsin contrast [%s],SD,Rod contrast [%s],SD,LMS contrast [%s],SD,L-M contrast [%s],SD\n', '%', '%', '%', '%', '%', '%', '%', '%');
 
 wls = SToWls([380 2 201]);
-load T_xyz1931
-T_xyz = SplineCmf(S_xyz1931,683*T_xyz1931,WlsToS(wls));
 
 currDir = pwd;
-LOG_PLOT = true;
-if LOG_PLOT
-    outFig1 = fullfile(outDir, 'FigureX_Spectra_log10.pdf');
-else
-    outFig1 = fullfile(outDir, 'FigureX_Spectra.pdf');
-end
+Mc = [];
 for d = 1:length(theDataPaths)
-    subplot(3, 2, d);
+    outFile = fullfile(outDir, ['TableX_Spectra' theStimuli{d} '_' theObservers{d} '.csv']);
     dataPath = theDataPaths{d};
     
     % Find the folders
     theFolders = dir(fullfile(dropboxBasePath, dataPath));
+    
+    % Increment the counter
+    clear bgSpdVal;
+    clear modSpdVal;
+    clear modSpdValMean;
+    clear modSpdValSD;
     
     for k = length(theFolders):-1:1
         % remove non-folders
@@ -50,42 +79,54 @@ for d = 1:length(theDataPaths)
     end
     
     % Iterate over the folders
-    cd(fullfile(dropboxBasePath, dataPath, theFolders(1).name));
-    
-    % Find the only MAT file there is going to be
-    theMATFile = dir([pwd '/*.mat']);
-    
-    theSpd = [];
-    
-    if ~isempty(theMATFile)
-        % Load the MAT file
-        tmp = load(theMATFile.name);
-        
-        % Get the background spectrum
-        bgSpd = tmp.cals{1}.modulationBGMeas.predictedSpd; hold on;ff
-        % Get the background spectrum
-        bgSpd = tmp.cals{1}.modulationBGMeas.meas.pr650.spectrum; hold on;
-        chromaticity{d}(1, :) = (T_xyz([1 2], :)*bgSpd)/sum((T_xyz*bgSpd));
-        
-        % Set up the color map
-        NContrastLevels = size(tmp.cals{1}.modulationAllMeas, 2)-1;
-        if NContrastLevels == 1
-            theRGB = copper(NContrastLevels+1);
-        else
-            theRGB = copper(NContrastLevels+1);
+    for f = 1:length(theFolders)
+        % Go to the folder
+        if isdir(fullfile(dropboxBasePath, dataPath, theFolders(f).name))
+            cd(fullfile(dropboxBasePath, dataPath, theFolders(f).name));
         end
-        for kk = 2:NContrastLevels+1
-            % Get the spectrum
-            modSpd = tmp.cals{1}.modulationAllMeas(1, kk).predictedSpd;
-            chromaticity{d}(kk, :) = (T_xyz([1 2], :)*modSpd)/sum((T_xyz*modSpd));
-            % ... and plot it.
-            if LOG_PLOT
-                plot(wls, log10(modSpd), 'Color', theRGB(kk, :)); hold on;
-            else
-                plot(wls, modSpd, 'Color', theRGB(kk, :)); hold on;
-            end
+        
+        % Find the only MAT file there is going to be
+        theMATFile = dir([pwd '/*.mat']);
+        
+        if ~isempty(theMATFile)
+            % Load the MAT file
+            tmp = load(theMATFile.name);
             
+            % Get the background spectrum
+            bgSpdVal(:, f) = tmp.cals{1}.modulationBGMeas.meas.pr650.spectrum; hold on;
+            plot(bgSpdNom); hold on
+            bgSpdNom = tmp.cals{1}.modulationBGMeas.predictedSpd;
+            
+            % Calculate the contrast
+            NContrastLevels = size(tmp.cals{end}.modulationAllMeas, 2)-1;
+            for kk = 2:NContrastLevels+1
+                modSpdVal{kk-1}(:, f) = tmp.cals{1}.modulationAllMeas(1, kk).meas.pr650.spectrum;
+                modSpdNom(:, kk-1)= tmp.cals{1}.modulationAllMeas(1, kk).predictedSpd;
+            end
+        end
+        
+        bgSpdValMean = mean(bgSpdVal, 2);
+        bgSpdValSD = std(bgSpdVal, [], 2);
+        
+        for kk = 1:NContrastLevels
+            modSpdValMean(:, kk) = mean(modSpdVal{kk}, 2);
+            modSpdValSD(:, kk) = std(modSpdVal{kk}, [], 2);
         end
     end
+    
+    % Gather all the spectra
+    theHeader = [theObservers{d} ', ' theStimuli{d}];
+    theString = 'Background nominal,Background validated,';
+    Mc = [];
+    Mc = [Mc bgSpdNom bgSpdValMean];
+    for ii = 1:NContrastLevels
+        theString = [theString sprintf('Modulation nominal [%i%s],Modulation validated [%i%s]', ...
+            theContrastLevels{d}(ii), '%', theContrastLevels{d}(ii), '%')];
+        Mc = [Mc modSpdNom(:, ii) modSpdValMean(:, ii)];
+    end
+    dlmwrite(outFile, Mc, '-append');
 end
+
+
 cd(currDir);
+fclose(fid);
