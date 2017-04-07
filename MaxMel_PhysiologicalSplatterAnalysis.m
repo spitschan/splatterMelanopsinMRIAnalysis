bb508b46462ae2d754276f78c265d64b564dece9
Manuel Spitschan <spitschan@stanford.edu>
2017-04-07 13:47:55 +0200

    Updates
    Signed-off-by: Manuel Spitschan <spitschan@stanford.edu>

--- MaxMel_PhysiologicalSplatterAnalysis.m
+++ MaxMel_PhysiologicalSplatterAnalysis.m
@@ -5,15 +5,10 @@ if ~isdir(outDir)
     mkdir(outDir);
 end
 
-theDataPaths = {'MELA_data/MelanopsinMR_fMRI/MaxMel400pct/HERO_asb1/032416/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableCStubby1_ND00/23-Mar-2016_12_31_27/validation' ...
-    'MELA_data/MelanopsinMR_fMRI/MaxMel400pct/HERO_aso1/032516/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableCStubby1_ND00/23-Mar-2016_12_31_27/validation' ...
-    'MELA_data/MelanopsinMR_fMRI/MaxMel400pct/HERO_gka1/033116/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableCStubby1_ND00/23-Mar-2016_12_31_27/validation' ...
-    'MELA_data/MelanopsinMR_fMRI/MaxMel400pct/HERO_mxs1/040616/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableCStubby1_ND00/23-Mar-2016_12_31_27/validation' ...
-    'MELA_data/MelanopsinMR_fMRI/MaxMelCRF/HERO_asb1/060716/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
+theDataPaths = {'MELA_data/MelanopsinMR_fMRI/MaxMelCRF/HERO_asb1/060716/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
     'MELA_data/MelanopsinMR_fMRI/MaxMelCRF/HERO_aso1/053116/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
     'MELA_data/MelanopsinMR_fMRI/MaxMelCRF/HERO_gka1/060216/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
     'MELA_data/MelanopsinMR_fMRI/MaxMelCRF/HERO_mxs1/060916/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
-    'MELA_data/MelanopsinMR_fMRI/MaxMelCRF/HERO_mxs1/061016/StimulusFiles/Cache-MelanopsinDirectedSuperMaxMel/BoxARandomizedLongCableBStubby1_ND00/29-May-2016_14_48_42/validation' ...
     'MELA_data/MelanopsinMR_fMRI/SplatterControlCRF/HERO_asb1/051016/StimulusFiles/Cache-MaxMelPostreceptoralSplatterControl/BoxARandomizedLongCableCStubby1_ND00/18-Apr-2016_10_23_32/validation' ...
     'MELA_data/MelanopsinMR_fMRI/SplatterControlCRF/HERO_aso1/042916/StimulusFiles/Cache-MaxMelPostreceptoralSplatterControl/BoxARandomizedLongCableCStubby1_ND00/18-Apr-2016_10_23_32/validation' ...
     'MELA_data/MelanopsinMR_fMRI/SplatterControlCRF/HERO_gka1/050616/StimulusFiles/Cache-MaxMelPostreceptoralSplatterControl/BoxARandomizedLongCableCStubby1_ND00/18-Apr-2016_10_23_32/validation' ...
@@ -104,53 +99,93 @@ for d = 1:length(theDataPaths)
     NSamples = 1000;
     receptorObj.makeSpectralSensitivitiesStochastic('NSamples', NSamples);
     
+    
+    T_receptors = receptorObj.T.T_energyNormalized;
+    for jj = 1:size(receptorObj.Ts{ii}.T_energyNormalized, 1)
+        contrastsFixed(jj) = (T_receptors(jj, :)*(modSpdValMean(:, end)-bgSpdValMean))./(T_receptors(jj, :)*bgSpdValMean);
+    end
+    postRecepContrastsFixed = [1 1 0 ; 1 -1 0 ; 0 0 1]' \ contrastsFixed';
+    
     for ii = 1:NSamples
         T_receptors = receptorObj.Ts{ii}.T_energyNormalized;
         for jj = 1:size(receptorObj.Ts{ii}.T_energyNormalized, 1)
-            contrasts(jj, ii) = (T_receptors(jj, :)*(modSpdValMean(:, end)-bgSpdValMean))./(T_receptors(jj, :)*bgSpdValMean);
+            contrastsStochastic(jj, ii) = (T_receptors(jj, :)*(modSpdValMean(:, end)-bgSpdValMean))./(T_receptors(jj, :)*bgSpdValMean);
         end
-        postRecepContrasts(:, ii) = [1 1 0 ; 1 -1 0 ; 0 0 1]' \ contrasts(:, ii);
+        postRecepContrastsStochastic(:, ii) = [1 1 1 ; 1 -1 0 ; 0 0 1]' \ contrastsStochastic(:, ii);
     end
     
-    theRGB = DefaultReceptorColors;
-    fig1 = figure;
-    XAxLims = [-0.1 0.1]; YAxLims = [-0.1 0.1];
-    XNominalContrast = 0; YNominalContrast = 0;
-    ScatterplotWithHistogram(postRecepContrasts(1, :), postRecepContrasts(2, :), ...
-        'XLim', XAxLims, 'YLim', YAxLims, 'XBinWidth', 0.01, 'YBinWidth', 0.01, ...
-        'XLabel', 'L+M contrast', 'YLabel', 'L-M contrast', ...
-        'XRefLines', [XAxLims ; YNominalContrast YNominalContrast], ...
-        'YRefLines', [XNominalContrast XNominalContrast ; YAxLims], ...
-        'XNominalContrast', XNominalContrast, ...
-        'YNominalContrast', YNominalContrast, ...
-        'Color', [theRGB(1, :) ; theRGB(2, :)]);
+    %     theRGB = DefaultReceptorColors;
+    %     fig1 = figure;
+    %     XAxLims = [-0.1 0.1]; YAxLims = [-0.1 0.1];
+    %     XNominalContrast = 0; YNominalContrast = 0;
+    %     ScatterPlotNoHistogram(postRecepContrastsStochastic(1, :), postRecepContrastsStochastic(2, :), ...
+    %         'XLim', XAxLims, 'YLim', YAxLims, 'XBinWidth', 0.01, 'YBinWidth', 0.01, ...
+    %         'XLabel', 'L+M+S contrast', 'YLabel', 'L-M contrast', ...
+    %         'XRefLines', [XAxLims ; YNominalContrast YNominalContrast], ...
+    %         'YRefLines', [XNominalContrast XNominalContrast ; YAxLims], ...
+    %         'XNominalContrast', XNominalContrast, ...
+    %         'YNominalContrast', YNominalContrast, ...
+    %         'Color', [theRGB(1, :) ; theRGB(2, :)]);
+    %     plot(postRecepContrastsFixed(1), postRecepContrastsFixed(2), 'xg', 'MarkerSize', 10);
+    %     title([theStimuli{d} '-' theObservers{d}]);
+    %
+    %     % Save the figure
+    %     set(fig1, 'PaperPosition', [0 0 3 3]);
+    %     set(fig1, 'PaperSize', [3 3]);
+    %     set(fig1, 'Color', 'w');
+    %     set(fig1, 'InvertHardcopy', 'off');
+    %     cd(currDir);
+    %     saveas(fig1, outFile1, 'png');
+    %
+    %     fig2 = figure;
+    %     XAxLims = [-0.1 0.1]; YAxLims = [-0.3 0.3];
+    %     XNominalContrast = 0; YNominalContrast = 0;
+    %     ScatterPlotNoHistogram(postRecepContrastsStochastic(1, :), postRecepContrastsStochastic(3, :), ...
+    %         'XLim', XAxLims, 'YLim', YAxLims, 'XBinWidth', 0.01, 'YBinWidth', 0.03, ...
+    %         'XLabel', 'L+M+S contrast', 'YLabel', 'S contrast', ...
+    %         'XRefLines', [XAxLims ; YNominalContrast YNominalContrast], ...
+    %         'YRefLines', [XNominalContrast XNominalContrast ; YAxLims], ...
+    %         'XNominalContrast', XNominalContrast, ...
+    %         'YNominalContrast', YNominalContrast, ...
+    %         'Color', [theRGB(1, :) ; theRGB(3, :)]);
+    %     plot(postRecepContrastsFixed(1), postRecepContrastsFixed(3), 'xg', 'MarkerSize', 10);
+    %     title([theStimuli{d} '-' theObservers{d}]);
+    %
+    %     % Save the figure
+    %     set(fig2, 'PaperPosition', [0 0 3 3]);
+    %     set(fig2, 'PaperSize', [3 3]);
+    %     set(fig2, 'Color', 'w');
+    %     set(fig2, 'InvertHardcopy', 'off');
+    %     saveas(fig2, outFile2, 'png');
+    %     close(fig1); close(fig2);
     
-    % Save the figure
-    set(fig1, 'PaperPosition', [0 0 6 6]);
-    set(fig1, 'PaperSize', [6 6]);
-    set(fig1, 'Color', 'w');
-    set(fig1, 'InvertHardcopy', 'off');
-    cd(currDir);
-    saveas(fig1, outFile1, 'png');
+    % Absolute
     
-    fig2 = figure;
-    XAxLims = [-0.1 0.1]; YAxLims = [-0.3 0.3];
-    XNominalContrast = 0; YNominalContrast = 0;
-    ScatterplotWithHistogram(postRecepContrasts(1, :), postRecepContrasts(3, :), ...
-        'XLim', XAxLims, 'YLim', YAxLims, 'XBinWidth', 0.01, 'YBinWidth', 0.03, ...
-        'XLabel', 'L+M contrast', 'YLabel', 'S contrast', ...
-        'XRefLines', [XAxLims ; YNominalContrast YNominalContrast], ...
-        'YRefLines', [XNominalContrast XNominalContrast ; YAxLims], ...
-        'XNominalContrast', XNominalContrast, ...
-        'YNominalContrast', YNominalContrast, ...
-        'Color', [theRGB(1, :) ; theRGB(3, :)]);
+    subplot(1, 2, 1);
+    if d < 5
+        plot(abs(postRecepContrastsStochastic(1, :)), abs(postRecepContrastsStochastic(2, :)), '.k'); hold on;
+    else
+        plot(abs(postRecepContrastsStochastic(1, :)), abs(postRecepContrastsStochastic(2, :)), '.r'); hold on;
+    end
+    xlim([-0.0025 0.1]); xlabel('L+M+S contrast');
+    ylim([-0.0025 0.1]); ylabel('L-M contrast');
+    pbaspect([1 1 1]); set(gca, 'TickDir', 'out'); box off;
     
-    % Save the figure
-    set(fig2, 'PaperPosition', [0 0 6 6]);
-    set(fig2, 'PaperSize', [6 6]);
-    set(fig2, 'Color', 'w');
-    set(fig2, 'InvertHardcopy', 'off');
-    saveas(fig2, outFile2, 'png');
-    close(fig1); close(fig2);
-    
+    subplot(1, 2, 2);
+    if d < 5
+        plot(abs(postRecepContrastsStochastic(1, :)), abs(postRecepContrastsStochastic(3, :)), '.k'); hold on;
+    else
+        plot(abs(postRecepContrastsStochastic(1, :)), abs(postRecepContrastsStochastic(3, :)), '.r'); hold on;
+    end
+    xlim([-0.0025 0.1]); xlabel('L+M+S contrast');
+    ylim([-0.0075 0.3]); ylabel('S contrast');
+    pbaspect([1 1 1]); set(gca, 'TickDir', 'out'); box off;
+    ls
 end
+        set(gcf, 'PaperPosition', [0 0 6 3]);
+        set(gcf, 'PaperSize', [6 3]);
+        set(gcf, 'Color', 'w');
+        set(gcf, 'InvertHardcopy', 'off');
+        saveas(gcf, '~/Desktop/Test.png', 'png');
+
+ls
